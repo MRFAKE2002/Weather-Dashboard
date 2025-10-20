8df2c9f43544fdb9224891f1ee129985781971d9
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-10-20 12:09:47 +0330

    Add our pages and routes in app

--- Weather-Dashboard/src/App.tsx
+++ Weather-Dashboard/src/App.tsx
@@ -1,10 +1,48 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import AppProviders from "./components/layout/AppProviders";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
+import AuthLayout from "./components/layout/AuthLayout";
+import MainLayout from "./components/layout/MainLayout";
+import RequireAuth from "./routes/RequireAuth";
 
-function App() {
+export default function App() {
+  const userName = localStorage.getItem("name");
 
   return (
-    <>
-    </>
-  )
-}
+    <AppProviders>
+      <Routes>
+        {/* There is not Header or Footer for these routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
 
-export default App
+        {/* There is Header and Footer for these routes */}
+        <Route
+          element={
+            <RequireAuth>
+              <MainLayout />
+            </RequireAuth>
+          }
+        >
+          <Route
+            path="/"
+            element={
+              <Navigate to={userName ? "/dashboard" : "/login"} replace />
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <RequireAuth>
+                <DashboardPage />
+              </RequireAuth>
+            }
+          />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AppProviders>
+  );
+}
